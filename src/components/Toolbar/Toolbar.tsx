1c622248b79c421834ed6b36c5ed47162ccebd9c
Zhuravlev Petr <zeterfrost@gmail.com>
2023-12-02 11:58:47 +0600

    created NewQuote and Toolbar component

---
+++ src/components/Toolbar/Toolbar.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {Link, NavLink} from 'react-router-dom';
+
+const Toolbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">Quotes Central</Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <NavLink to="/" className="nav-link">Quotes</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/add-quote" className="nav-link">Submit new quote</NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Toolbar;
--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,7 +1,23 @@
+import Toolbar from '../../components/Toolbar/Toolbar';
+import {Route, Routes} from 'react-router-dom';
+import NewQuote from '../../components/NewQuote/NewQuote';
+
 const App = () => {
   return (
     <>
-      <div className="text-bg-dark">Hello</div>
+      <header>
+        <Toolbar/>
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={(
+            <h1>1</h1>
+          )}/>
+          <Route path="/add-quote" element={(
+            <NewQuote/>
+          )}/>
+        </Routes>
+      </main>
     </>
   );
 };
